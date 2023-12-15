814572d0a782fc7ecfcdc394800958972ae69da3
Artiom Bozieac <artiomytprofile@gmail.com>
2023-12-15 22:54:00 +0200

    Add class to manage application properties

---
+++ src/main/java/artiom/bozieac/utils/ApplicationProperties.java
@@ -0,0 +1,52 @@
+package artiom.bozieac.utils;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Class that manages application properties.
+ */
+public class ApplicationProperties {
+
+    /**
+     * Logger instance.
+     */
+    private static final Logger logger = LogManager.getLogger(ApplicationProperties.class);
+
+    /**
+     * Path to the application's properties.
+     */
+    private static final String APPLICATION_PROPERTIES_PATH = "src/main/resources/application.properties";
+
+    /**
+     * Application properties object.
+     */
+    private static final Properties applicationProperties = new Properties();
+
+    /**
+     * Static initializer to read the properties from disk.
+     */
+    static {
+        try {
+            applicationProperties.load(new FileInputStream(APPLICATION_PROPERTIES_PATH));
+        } catch (IOException e) {
+            if (logger.isErrorEnabled()) {
+                logger.error("Properties couldn't be loaded.", e);
+            }
+        }
+    }
+
+    /**
+     * Returns a property.
+     *
+     * @param propertyName - Property's name.
+     * @return The property.
+     */
+    public static String getProperty(final String propertyName) {
+        return applicationProperties.getProperty(propertyName);
+    }
+}
---
+++ src/main/java/artiom/bozieac/utils/ApplicationPropertiesConstants.java
@@ -0,0 +1,22 @@
+package artiom.bozieac.utils;
+
+/**
+ * Constants for application properties.
+ */
+public class ApplicationPropertiesConstants {
+
+    /**
+     * Constant for DB url property.
+     */
+    public static final String DB_URL = "db_url";
+
+    /**
+     * Constant for DB username property.
+     */
+    public static final String DB_USERNAME = "db_username";
+
+    /**
+     * Constant for DB password property.
+     */
+    public static final String DB_PASSWORD = "db_password";
+}
